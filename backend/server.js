43e4d1c94a12dc1624adc76ee62bda21bc362fd9
Pratham Goel <pratham.goel.22.183@gmail.com>
2025-01-03 00:11:58 +0530

    vercel.json

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,6 @@ import projectModel from './models/project.model.js';
 import { generateResult } from './services/ai.service.js';
 
 const port = process.env.PORT || 3000;
-
-
-
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
